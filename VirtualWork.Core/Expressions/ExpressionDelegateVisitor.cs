f7eab25ba2c5483762d81e4c04b984731f2040cf
Mortens4444 <mortens.4444@gmail.com>
2020-10-04 23:01:36 +0200

    Use virtual items in log list view

---
+++ VirtualWork.Core/Expressions/ExpressionDelegateVisitor.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq.Expressions;
+
+namespace VirtualWork.Core.Expressions
+{
+	internal sealed class ExpressionDelegateVisitor : ExpressionVisitor
+	{
+
+		private readonly Func<Expression, Expression> visitor;
+		private readonly bool recursive;
+
+		public static Expression Visit(Expression exp, Func<Expression, Expression> visitor, bool recursive)
+		{
+			return new ExpressionDelegateVisitor(visitor, recursive).Visit(exp);
+		}
+
+		private ExpressionDelegateVisitor(Func<Expression, Expression> visitor, bool recursive)
+		{
+			this.visitor = visitor ?? throw new ArgumentNullException(nameof(visitor));
+			this.recursive = recursive;
+		}
+
+		public override Expression Visit(Expression node)
+		{
+			if (recursive)
+			{
+				return base.Visit(visitor(node));
+			}
+			else
+			{
+				var visited = visitor(node);
+				if (visited == node)
+				{
+					return base.Visit(visited);
+				}
+				return visited;
+			}
+		}
+	}
+}
--- VirtualWork.WinForms/LogViewer.cs
+++ VirtualWork.WinForms/LogViewer.cs
@@ -1,10 +1,18 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.ComponentModel;
 using System.Drawing;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Windows.Forms;
 using LanguageService;
 using LanguageService.Windows.Forms;
+using VirtualWork.Core.Extensions;
+using VirtualWork.Core.Log;
+using VirtualWork.Core.Utils;
+using VirtualWork.Interfaces.Enums;
+using VirtualWork.WinForms.Extensions;
+using DbLogEntry = VirtualWork.Persistence.Entities.LogEntry;
 using LogEntryRepository = VirtualWork.Persistence.Repositories.RepositoryBase<VirtualWork.Core.Log.LogEntry, VirtualWork.Persistence.Entities.LogEntry>;
 
 namespace VirtualWork.WinForms
@@ -13,6 +21,7 @@ namespace VirtualWork.WinForms
 	{
 		private readonly LogEntryRepository logEntryRepository;
 		private readonly ExceptionViewer exceptionViewer;
+		private IList<ListViewItem> logEntries;
 
 		public LogViewer(LogEntryRepository logEntryRepository,
 			ExceptionViewer exceptionViewer)
@@ -22,38 +31,85 @@ namespace VirtualWork.WinForms
 
 			InitializeComponent();
 			Translator.Translate(this);
+
+			cbLogLevel.Items.Add(Lng.Elem("All"));
+			cbLogLevel.FillWithEnum<LogLevel>();
+			GetLogItems();
 		}
 
-		private void LogViewer_Shown(object sender, EventArgs e)
+		private void GetLogItems(Func<Persistence.Entities.LogEntry, bool> predicate = null)
 		{
-			lvLogs.Items.Clear();
-			var logEntries = logEntryRepository.GetAll().OrderByDescending(logEntry => logEntry.Id);
+			var items = logEntryRepository.GetAll(predicate);
+			var convertedItems = items.Select((item, index) => { return LogEntryToListViewItem(item, index); });
+			logEntries = convertedItems.OrderByDescending(listviewItem => ((LogEntry)listviewItem.Tag).Id).ToList();
+			lvLogs.VirtualListSize = logEntries.Count;
+		}
 
-			int i = 0;
-			foreach (var logEntry in logEntries)
+		private ListViewItem LogEntryToListViewItem(LogEntry logEntry, int itemIndex)
+		{
+			var backColor = itemIndex % 2 == 0 ? lvLogs.BackColor : Color.LightBlue;
+			var listViewItem = new ListViewItem(logEntry.Id.ToString())
 			{
-				var backColor = i % 2 == 0 ? lvLogs.BackColor : Color.LightBlue;
-				var listViewItem = new ListViewItem(logEntry.Id.ToString())
-				{
-					BackColor = backColor					
-				};
-				listViewItem.SubItems.Add(logEntry.User?.ToString() ?? String.Empty);
-				listViewItem.SubItems.Add(Lng.Elem(logEntry.LogLevel.ToString()));
-				listViewItem.SubItems.Add(logEntry.Message);
-				lvLogs.Items.Add(listViewItem);
-				i++;
-			}
+				BackColor = backColor
+			};
+			listViewItem.SubItems.Add(logEntry.User?.ToString() ?? String.Empty);
+			listViewItem.SubItems.Add(Lng.Elem(logEntry.LogLevel.ToString()));
+			listViewItem.SubItems.Add(logEntry.Message);
+			listViewItem.Tag = logEntry;
+			return listViewItem;
 		}
 
 		private void TsmiExceptionViewer_Click(object sender, EventArgs e)
 		{
-			var selectedItem = lvLogs.SelectedItems[0];
+			var selectedItem = logEntries[lvLogs.SelectedIndices[0]];
 			exceptionViewer.Show(selectedItem.SubItems[3].Text);
 		}
 
 		private void ContextMenuStrip_Opening(object sender, CancelEventArgs e)
 		{
-			tsmiExceptionViewer.Enabled = lvLogs.SelectedItems.Count == 1;
+			tsmiExceptionViewer.Enabled = lvLogs.SelectedIndices.Count == 1;
+		}
+
+		private void LvLogs_RetrieveVirtualItem(object sender, RetrieveVirtualItemEventArgs e)
+		{
+			e.Item = logEntries[e.ItemIndex];
+		}
+
+		private bool IsFiltered()
+		{
+			return tbMessage.Text != String.Empty || cbUser.Text != String.Empty || cbLogLevel.Text != String.Empty;
+		}
+
+		private void FiltersChanged(object sender, EventArgs e)
+		{
+			GetItems();
+		}
+
+		private void GetItems()
+		{
+			Expression<Func<DbLogEntry, bool>> resultExpression = entity => true;
+			Expression<Func<DbLogEntry, bool>> messageFilter = entity => entity.Message.ContainsIgnoreCase(tbMessage.Text);
+			Expression<Func<DbLogEntry, bool>> userFilter = entity => entity.User != null && entity.User.FullName.ContainsIgnoreCase(cbUser.Text) || entity.User.Name.ContainsIgnoreCase(cbUser.Text);
+			Expression<Func<DbLogEntry, bool>> logLevelFilter = entity => entity.LogLevel == (int)EnumUtils.GetEnumValue<LogLevel>(cbLogLevel.Text);
+
+			if (tbMessage.Text != String.Empty)
+			{
+				resultExpression = resultExpression.And(messageFilter);
+			}
+			if (cbUser.Text != String.Empty)
+			{
+				resultExpression = resultExpression.And(userFilter);
+			}
+			else if (cbLogLevel.SelectedIndex != 0)
+			{
+				resultExpression = resultExpression.And(logLevelFilter);
+			}
+			GetLogItems(resultExpression.Compile());
 		}
 	}
+
+
+
+
+
 }
